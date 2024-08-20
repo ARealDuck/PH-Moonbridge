ced859379d8838655dfa921a02d50151189d6932
A_Duck <therealaduck@gmail.com>
2024-08-20 13:49:08 -0500

    learning C++ stuff is hard...

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <ctime>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 
 // The Logger Object to be created
@@ -43,6 +47,7 @@ private:
 
     // Block to set logmessage data member
     // NOTE: data member should be set by the createlog function within the public class section.
+    string logmessage;
 
     // Block to set the finallog data member
     // This should be created upon program launch
@@ -56,8 +61,20 @@ private:
     vector<finallog> logstorage;
 
 public:
-    //function to call to add logs to the logger
-    void logging() {
-        
+    // function to call to add logs to the logger
+    void logging(loglevels) {
+        updatelogtime();
+
     }
+
+    // function to call to grab the current time to set for logtime
+    void updatelogtime() {
+        auto now = chrono::system_clock::now();
+        time_t currentTime = chrono::system_clock::to_time_t(now);
+        ostringstream oss;
+        oss << put_time(localtime(&currentTime), "%H:%M:%S");
+        logtime = oss.str();
+    }
+    
+    // 
 };

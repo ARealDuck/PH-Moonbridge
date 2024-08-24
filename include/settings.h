91dd46f06fad40d644906809c7ca94778c3a6825
A_Duck <therealaduck@gmail.com>
2024-08-23 20:30:59 -0500

    cleaning up a bit.

--- include/settings.h
+++ include/settings.h
@@ -4,30 +4,31 @@
 class settings
 {
 private:
-// constructor to init settings
+// settings constructor
 settings();
 // Load all settings from the designated json file
 // On failure, throws a WARN level message to the logger and create a new json settings file from the defaults
-static void loadallsettings() {
+    static void loadallsettings() {
 
-}
+    }
 
-static void saveallsettings() {
+    static void saveallsettings() {
 
-}
+    }
 
 public:
-// settings contructor
-static settings settingsinit();
+// settings contructor init method
+    static settings settingsinit() {
+    }
+
+    static settings setsetting() {
+
+    }
+    static settings savesetting() {
+
+    }
+    static settings loadsetting() {
+
+}
 };
-static settings getsetting() {
-
-}
-static settings savesetting() {
-
-}
-static settings loadsetting() {
-
-}
-
 #endif
---
+++ src/Core.cpp
@@ -0,0 +1,9 @@
+// all header files used in the core
+#include "Logger.h"
+#include "settings.h"
+// any library dependancies used within the core
+
+int main()
+{
+
+};

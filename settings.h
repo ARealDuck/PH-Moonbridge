2350b3c4e71813a2590939f8876408b70b919c64
A_Duck <therealaduck@gmail.com>
2024-09-01 08:35:15 -0500

    I have a new idea on how to handle this, so I wiped it clean.

--- settings.h
+++ settings.h
@@ -1,40 +1,24 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
-#include <vector>
-#include <nlohmann/json.hpp>
-using namespace std;
+
+#include <nlohmann/json>
+#include <string>
 class settings
 {
-private:
-    //privately declared methods
-    //descriptions are found in the source file.
-    void loadsettings();
-    static void defaultsettings();
-    string filename;
-    json settingsdata;
-    //mutable shared_mutex settingsMutex;
-
-    // settings constructor
-    // see settingsinit for description.
-    settings(const string& filename);
-    ~settings();
-    settings(const settings&) = delete;
-        settings& operator=(const settings&) = delete;
-
 public:
-    //Interface
-    static settings& getInstance(const string& filename = "settings.json")
-        //publicly declared methods
-        //descriptions are found in the source file.
-        template<typename T>
-    t getsetting(const string& identifier);
-    void savesettings();
+	std::string loadsettings(std::string& filename);
+	void savesettings();
+	std::string getsetting(std::string& identifier);
 
-    // settings contructor init method
-    // used to load the settings object and calls loadallsettings to load the settings.json file into memory.
-    static settings settingsinit(){
 
-    }
+private:
+	settings(std::string& filename) {
+
+	}
+	std::string filename;
+	std::string identifier;
+	nlohmann::json loadedsettings;
 
 };
-#endif
+
+#endif // !SETTINGS_H

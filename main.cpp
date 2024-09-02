8f718102f6ec6781abc983be0bfc69b3bad4ddf0
A_Duck <therealaduck@gmail.com>
2024-09-02 17:21:23 -0500

    cleaning up

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-// all header files used in the core
+// main.cpp is the main runtime file in the project
+// TODO: Work and finish all functions
 #include "logger.h"
 #include "settings.h"
-// any library dependancies used within the core
+
 
 int main()
 {
+	//main runtime function
+	int runtime(); {
+		
+	}
+	void moduleptrsend(); {
 
+	}
 };

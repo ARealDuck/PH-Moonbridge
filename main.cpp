ab77fc28be8b915106d822dec33ad283ec6430d9
A_Duck <therealaduck@gmail.com>
2024-09-06 20:02:03 -0500

    clean this up a bit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 // main.cpp is the main runtime file in the project
 // TODO: Work and finish all modules
 // Includes
-#include <vector>
 // Modules
 #include "logger.hpp"
 #include "settings.hpp"
+#include "obsws.hpp"
 
 using namespace std;
-// TODO: add a function to find this file inside of the settings module
-// for now its okay being a hardcoded location.
-string filename = "settings.json";
+
 
 void static runtimeinit() {
-
-	settings& settings = settings::settingsinit(filename);
 	logger* logger = logger::loggerinit();
+	settings* settings = settings::settingsinit();
+	obsws* obsws = obsws::obswsinit();
+}
 
+int main() {
+	runtimeinit();
 }

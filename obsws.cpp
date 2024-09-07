ccf2b71be4a5e878c40b3ff3d750fcd3a927e1e6
A_Duck <therealaduck@gmail.com>
2024-09-07 16:08:04 -0500

    almost done with this.

--- obsws.cpp
+++ obsws.cpp
@@ -2,12 +2,14 @@
 #include "settings.hpp"
 #include "logger.hpp"
 #include <iostream>
-#include "cryptopp/base64.h"
-#include "cryptopp/sha.h"
-#include "cryptopp/filters.h"
-#include "cryptopp/files.h"
+#include <cryptopp/base64.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/files.h>
 
+using namespace std;
 using namespace CryptoPP;
+using json = nlohmann::json;
 // init
 obsws* obsws::instance = nullptr;
 obsws* obsws::obswsinit() {
@@ -17,16 +19,73 @@ obsws* obsws::obswsinit() {
 	return instance;
 }
 // contructor
-obsws::obsws() {
+obsws::obsws() { 
 	settings = settings::settingsinit();
 	logger = logger::loggerinit();
+	c.init_asio();
+	c.set_message_handler([this](websocketpp::connection_hdl hdl, client::message_ptr msg) {
+		this->on_message(hdl, msg);
+		});
+	
+	logger->add(debug, "OBSws Module constructed, starting payload...");
+	startws();
 }
 // destructor
 obsws::~obsws() {
+	stopws();
+}
+
+// Handler for reciving messages from OBS Websocket
+void obsws::on_message(websocketpp::connection_hdl hdl, client::message_ptr msg) {
+	string payload = msg->get_payload();
+	json jsonmsg = json::parse(payload);
+	if (jsonmsg.contains("op")) {
+		int opcode = jsonmsg["op"];
+		msghndlr(opcode, jsonmsg);
+		}
+	else {
+		logger->add(error, "Unknown OBS Websocket OpCode:" + opcode);
+	}
+}
+
+// Starts Websocket client to connect to OBS websocket with
+void obsws::startws() {
+	logger->add(info, "Connecting to OBS-WebSocket...");
+	string wsurl = "ws://localhost:" + settings->cache<string>("port");
+	websocketpp::lib::error_code ec;
+	con = c.get_connection(wsurl, ec);
+	if (ec) {
+		logger->add(error, "could not establish connection: " + ec.message());
+		return;
+	}
+	c.connect(con);
+	//starts io loop on seperate thread
+	eventloopthread = thread([this]() {
+		c.run();
+		});
+		// waits for the response from OBS then lets the loop run
+		std::unique_lock<mutex> lock(mtx);
+		cv.wait(lock, [this]() { return connected; });
+		logger->add(info, "Successfully connected to OBS Websocket!");
+}
+
+// Closes Websocket Client
+void obsws::stopws() {
+	c.stop();
+	if (eventloopthread.joinable()) {
+		eventloopthread.join();
+	}
+	logger->add(debug, "OBS Websocket client closed.");
+}
+
+
+
+// Sends messages to the OBS websocket server
+void obsws::sendmsg() {
 
 }
 
-//auth
+// Creates OBE Websockets auth key when requrired
 string obsws::auth(string& challenge, string& salt) {
 	string password = settings->cache<string>("password");
 	stringstream passsalt;
@@ -44,3 +103,21 @@ string obsws::auth(string& challenge, string& salt) {
 	StringSource(input, true, new HashFilter(hash, new Base64Encoder(new StringSink(digest))));
 	return digest;
 }
+
+// Handles any messages recieved by on_message
+void obsws::msghndlr(int opcode,const json& jsonmsg) {
+	if (opcode == 0) {
+		opcode0(jsonmsg);
+	}
+	else if (opcode ==  {
+	
+	}
+}
+void opcode0(const json& msg) {
+
+}
+
+ // Example for return messages
+json returnmsg;
+returnmsg["op"] = 1;
+returnmsg["d"] = {{}
--- obsws.hpp
+++ obsws.hpp
@@ -6,21 +6,38 @@
 #include "settings.hpp"
 #include "logger.hpp"
 #include "nlohmann/json.hpp"
+#include <websocketpp/client.hpp>
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <unordered_map>
+#include <functional>
 // shorteners
 using namespace std;
+
 
 class obsws
 {
 public:
 	// Module Flags
-
+	bool connected = false;
 	// Init Function
 	static obsws* obswsinit();
 	// Module Pointer
 	static obsws* instance;
 	// External module functions & containers
-	void request();
+	void sendmsg();
 private:
+	// internal flags
+	typedef websocketpp::client<websocketpp::config::asio_client> client;
+	websocketpp::client<websocketpp::config::asio_client>::connection_ptr con;
+	client c;
+	std::mutex mtx;
+	std::condition_variable cv;
+	bool connected = false;
+	std::thread eventloopthread;
+	unordered_map<int, opcodehandler> handlers;
 	// Module Dependencies
 	settings* settings;
 	logger* logger;
@@ -31,8 +48,11 @@ private:
 	obsws& operator=(const obsws&) = delete;
 	// Internal module functions & containers
 	string auth(string& challenge, string& salt);
-	void connect();
-	void disconnect();
+	void on_message(websocketpp::connection_hdl hdl, client::message_ptr msg);
+	void startws();
+	void stopws();
+	void msghndlr(int opcode, const json& jsonmsg);
+
 	
 };
 

f63fcff7cd17ba7f0ef30abc4837191fda89c40c
A_Duck <therealaduck@gmail.com>
2024-10-04 04:07:56 -0500

    damn bro this shit got hands

--- taskscheduler.cpp
+++ taskscheduler.cpp
@@ -4,6 +4,7 @@
 // Constructor
 TaskScheduler::TaskScheduler(int threadcount) : threadcount_(threadcount) {
 	taskqueues.resize(threadcount);
+	ioloopactive.resize(threadcount, false);
 }
 
 // Destructor, finishes current ongoing tasks, and ensure all threads are properly joined before exiting.
@@ -11,23 +12,41 @@ TaskScheduler::~TaskScheduler() {
 	// TODO: implement this block of code
 }
 
-// adds tasks to the task queue
-
-void TaskScheduler::addtask(std::function<void()> task, int threadID, int flags = functionflags::NONE, int forcedthreadID = -1) {
-	std::lock_guard<std::mutex> lock(mtx);
-	taskqueues[threadID].push({ task,flags,forcedthreadID });
-	cv.notify_all();
+// pass function or method into the task queue
+void TaskScheduler::executefunc(std::function<void()> task, int threadID, int flags = functionflags::NONE) {
+	std::lock_guard <std::mutex> lock(mtx);
+	// check if thread is protected or IOLooped
+	if (flags & functionflags::IO_LOOP) {
+		ioloopactive[threadID] = true;
+	}
+	// if thread is locked disallow queuing of tasks in that queue
+	if (!ioloopactive[threadID]) {
+		taskqueues[threadID].push({ task, flags, threadID });
+		cv.notify_all();
+	}
 }
 
 void TaskScheduler::threadworker(int threadID) {
-	while (true) { 
+	while (true) {
 		std::unique_lock<std::mutex> lock(mtx);
-		// waits for task to be available to be worked on.
+		// waits till task becomes available to be executed on the current thread
 		cv.wait(lock, [&]() {return !taskqueues[threadID].empty(); });
 
-		auto task = taskqueues[threadID].front();
-		taskqueues[threadID].pop();
-		lock.unlock();
+		// if thread is not locked execute a task
+		while (!taskqueues[threadID].empty()) {
+			auto task = taskqueues[threadID].front();
+			taskqueues[threadID].pop();
+			lock.unlock();
 		}
-	}
+		if (task.flags & functionflags::IO_LOOP) {
+			// logger debug logmessage: "starting I/O loop on thread: " + threadID
+			task.func();
+			ioloopactive[threadID] = true;
+		}
+		else {
+			task.func();
+		}
+		lock.lock();
+}
+
 }
--- taskscheduler.h
+++ taskscheduler.h
@@ -1,53 +1,37 @@
 #ifndef TASKSCHEDULER_H
 #define TASKSCHEDULER_H
 
-#include <iostream>
-#include <thread>
 #include <functional>
-#include <vector>
 #include <queue>
+#include <thread>
 #include <mutex>
 #include <condition_variable>
-#include <unordered_map>
+#include <vector>
+#include <atomic>
 
 class TaskScheduler {
 public:
-	// Flags for tasks when registering them
-	enum functionflags {
-		NONE = 0,
-		// If the task starts an I/O loop
-		IO_LOOP = 1 << 0,
-		// Forces any task to be ran on this thread by default
-		FORCED_THREAD = 1 << 1, 
-	};
-	
-	// Structure for registered tasks
-	struct tasks {
-		std::function<void()> func;
-		int flags;
-		// only used if forced_thread flag is set.
-		int forcedthreadID;
-	};
-
-	// task queue for each thread
-	std::vector<std::queue<tasks>> taskqueues;
-	std::mutex mtx;
-	std::condition_variable cv;
-	TaskScheduler(int threadcount);
+	// constructs the taskscheduler with a default of 4 threads
+	TaskScheduler(size_t numthreads = 4);
+	// destructor for graceful shutdown of threads
 	~TaskScheduler();
 
-	// adds a task with flags if needed
-	void addtask(std::function<void()> task, int threadID, int flags = functionflags::NONE, int forcedthreadID = -1);
+	// queues a task
+	void queuetask(std::function<void()> task);
 
-	void startthreads();
-
-	void threadworker(int threadID);
+	// stops the scheduler
+	void stop();
 
 private:
-	int threadcount_;
-
+	// storage for worker threads
+	std::vector<std::thread> wthreads;
+	// Task queue
+	std::vector<std::function<void()>> tasks;
+	std::mutex queuemtx;
+	std::condition_variable cv;
+	std::atomic <bool> stopflag;
+	void workerthread();
 };
-
 extern TaskScheduler gtaskscheduler;
 
 #define ts gtaskscheduler

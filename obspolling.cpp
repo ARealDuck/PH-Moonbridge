40d6ed91f3164e897fc603801c395ddf0129e8e3
A_Duck <therealaduck@gmail.com>
2024-11-07 04:07:51 -0600

    baubaubaubaubaubau

--- obspolling.cpp
+++ obspolling.cpp
@@ -5,11 +5,30 @@ obspolling::obspolling() {}
 void obspolling::startpolling() {
 	gthreadpool.enqueueTask([this] {	
 		pollingclient.connect(syncdata);
-
 		running = true;
-		pollingclient
+		while (running) {
+			imagedata = getimagedata();
+			// wait for 500ms before sending request again
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
 	});
 
 }
 
-obspolling obspoll
+std::string obspolling::getimagedata() {
+	nlohmann::json datamsg;
+	datamsg["op"] = 6;
+	datamsg["d"] = {
+		{"requestType", "GetSourceScreenshot"},
+		{"requestId", pollingclient.createid() },
+		{"requestData", {
+			{"sourceName", sourcename},
+			{"imageFormat", "png"},
+			{"imageCompressionQuality", 100}
+		}}
+	};
+	nlohmann::json returnmsg = pollingclient.sendmsg(datamsg);
+	returnmsg.parse
+}
+
+obspolling obspoll;
--- wsclient.cpp
+++ wsclient.cpp
@@ -5,7 +5,7 @@
 #include <cryptopp/filters.h>
 #include <cryptopp/files.h>
 
-wsClient::wsClient() : ws_client_(), responseready(false) {
+wsClient::wsClient() : ws_client_() {
 	ws_client_.init_asio(&tunnel.getiocontext());
 	ws_client_.start_perpetual();
 	ws_client_.set_message_handler([this](websocketpp::connection_hdl, client::message_ptr msg) {
@@ -14,7 +14,7 @@ wsClient::wsClient() : ws_client_(), responseready(false) {
 
 }
 
-void wsClient::connect() {
+void wsClient::connect(clientsync& syncdata) {
 	settingsreader wsSettingshelper("obswebsocket", globalsettings);
 	websocketpp::lib::error_code ec;
 	std::string port;
@@ -27,9 +27,11 @@ void wsClient::connect() {
 		}
 		handle = con->get_handle();
 		ws_client_.connect(con);
-		if (!responseready) {
-			
-	}
+		{ 
+			std::lock_guard<std::mutex> lock(syncdata.clientmtx);
+			syncdata.ready = true;
+		}
+		syncdata.clientcv.notify_one();
 }
 
 nlohmann::json wsClient::sendmsg(const nlohmann::json& message) {
@@ -83,6 +85,12 @@ void wsClient::msghandle(const std::string& reponse) {
 
 bool wsClient::checkcon() {
 	return handshake;
+}
+
+std::string wsClient::createid() {
+	auto now = std::chrono::system_clock::now();
+	auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+	return "req" + std::to_string(millis);
 }
 
 std::string wsClient::createauth(std::string& challenge, std::string& salt) {

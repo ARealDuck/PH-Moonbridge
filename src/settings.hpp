6e744413139c1284d6109eab13d2f566e1a8e325
A_Duck <therealaduck@gmail.com>
2025-01-30 03:45:47 -0600

    Comments sums up how I feel about working on this for now go read that.

--- src/settings.hpp
+++ src/settings.hpp
@@ -1,43 +1,7 @@
 #ifndef SETTINGS_HPP
-#include <nlohmann/json.hpp>
-#include <iostream>
-#include <fstream>
-#include "main.cpp"
+#define SETTINGS_HPP
+// Im not even going to bother creating this now as I dont even know how complex im going to need this to be.
+// I could go extremely overkill with this but right now thats far too much effort for someone doing this for less than an hour on their lunch break...
 
-class settings {
-public:
-	settings();
-	std::string grab(SettingID id);
-	void set(std::string string_value);
-	bool load();
-	void save();
-	// Setting keys to be used when grabbing settings using the "grab" and "set" methods.
-	enum SettingID {
-		OBS_PASSWORD,
-		OBS_PORT,
-		OBS_URL,
-		BREAK
-	};
-
-
-private:
-	bool check(std::vector<std::string>& keys);
-	void apply(SettingID id, const nlohmann::json jsonfile);
-	void defaults();
-	std::string idtostring(SettingID id) const {
-		return grab(id)
-	}
-	std::vector<std::string> SettingKeys = {
-		"obs_password", "obs_port", "obs_url"
-	};
-	struct SETTINGS_DEFAULT {
-		std::string obs_password = "unsafepassword";
-		std::string obs_port = "4455";
-		std::string obs_url = "localhost:";
-
-	};
-	SETTINGS_DEFAULT dsettings;
-	std::string filelocation = "settings.json";
-};
 
 #endif // !SETTINGS_HPP
